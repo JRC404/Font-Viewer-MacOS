dd1aa194c1a566c3ab6174c6aef77348c6602b7f
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-04-27 00:10:17 +0100

    MacOS application created.

---
+++ Font-Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Font-Viewer
+//
+//  Created by Jacob Reilly-Cooper on 27/04/2020.
+//  Copyright © 2020 Janapexo Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Font-Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Font-Viewer
+//
+//  Created by Jacob Reilly-Cooper on 27/04/2020.
+//  Copyright © 2020 Janapexo Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
